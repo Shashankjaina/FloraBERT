591e1358f1f4891a535be0b8df0ff7a7467425db
sravani charu <sravani.charu15@gmail.com>
2023-12-28 05:59:22 +0530

    firstcommit

---
+++ florabertps/src/Authenticate/ForgotPassword.js
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import { Modal, Form, InputGroup, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { auth} from './firebase'; // Update this import to the correct path
+import { db } from './firebase';
+import { sendPasswordResetEmail } from 'firebase/auth';
+function ForgotPassword() {
+    const [show, setShow] = useState(false);
+    const [email, setEmail] = useState('');
+    const [isResetSent, setIsResetSent] = useState(false);
+
+    const handleClose = () => {
+        setShow(false);
+        setIsResetSent(false);
+    };
+
+    const handleShow = () => {
+        setShow(true);
+        setIsResetSent(false);
+    };
+
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
+    };
+
+    const handleResetPassword = async () => {
+        try {
+            console.log(email);
+            await sendPasswordResetEmail(auth,email);
+            setIsResetSent(true);
+        } catch (error) {
+            console.error('Error sending reset password email:', error.message);
+        }
+    };
+    
+
+    return (
+        <div>
+            <Link to="#" onClick={handleShow}>
+                Forgot Password
+            </Link>
+
+            <Modal show={show} onHide={handleClose} centered>
+                <Modal.Header closeButton>
+                    <Modal.Title>Forgot Password?</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    {!isResetSent ? (
+                        <>
+                            <InputGroup className="mb-3">
+                                <InputGroup.Text id="inputGroup-sizing-default">
+                                    Email
+                                </InputGroup.Text>
+                                <Form.Control
+                                    type="email"
+                                    aria-label="Default"
+                                    aria-describedby="inputGroup-sizing-default"
+                                    value={email}
+                                    onChange={handleEmailChange}
+                                />
+                            </InputGroup>
+                            <Button variant="primary" onClick={handleResetPassword}>
+                                Reset Password
+                            </Button>
+                        </>
+                    ) : (
+                        <p>Password reset email sent. Check your email for further instructions.</p>
+                    )}
+                </Modal.Body>
+                <Modal.Footer>
+                    {/* You can add a button to close the modal if needed */}
+                    {/* <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button> */}
+                </Modal.Footer>
+            </Modal>
+        </div>
+    );
+}
+
+export default ForgotPassword;
---
+++ florabertps/src/Authenticate/firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDiN-RfPKBd_i2U5r71TJWK9U99s-LuFtw",
+  authDomain: "authenticatefbert.firebaseapp.com",
+  projectId: "authenticatefbert",
+  storageBucket: "authenticatefbert.appspot.com",
+  messagingSenderId: "353348803429",
+  appId: "1:353348803429:web:60f07eec13573d01435f8c",
+  measurementId: "G-XB0DLE0NBX"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
